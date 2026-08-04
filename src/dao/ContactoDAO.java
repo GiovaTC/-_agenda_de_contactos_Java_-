ea4dd47497cdf36be3818c35c15f7fdbaf874970
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-08-04 16:05:21 -0500

    commit - ContactoDAO .

---
+++ src/dao/ContactoDAO.java
@@ -0,0 +1,27 @@
+package dao;
+
+import config.ConexionBD;
+import modelo.Contacto;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * ==========================================================
+ * Clase: ContactoDAO
+ * Proyecto: Agenda de Contactos
+ * Autor: Giovanny A. Tapiero C. :. . /  .
+ * Java: 21
+ * Base de Datos: MySQL 8
+ * ==========================================================
+ *
+ * Responsabilidad:
+ * Administrar las operaciones CRUD de la tabla contactos.
+ */
+
+public class ContactoDAO {
+
+
+}
