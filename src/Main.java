0fd644b7a7fa5fa6b541825c78b14040c6cd0280
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-08-04 16:38:45 -0500

    commit - Main .

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,82 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import dao.ContactoDAO;
+import modelo.Contacto;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+import java.util.Scanner;
+
+/**
+ * ==========================================================
+ * Clase: Main
+ * Proyecto: Agenda de Contactos
+ * Autor: Giovanny A. Tapiero C. :. . /  .
+ * Java: 21
+ * Base de Datos: MySQL 8
+ * ==========================================================
+ *
+ * Programa principal.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        ContactoDAO contactoDAO = new ContactoDAO();
+
+        int opcion;
+
+        do {
+            System.out.println();
+            System.out.println("=================================");
+            System.out.println("      AGENDA DE CONTACTOS");
+            System.out.println("=================================");
+            System.out.println("1. Agregar contacto");
+            System.out.println("2. Listar contactos");
+            System.out.println("3. Salir");
+            System.out.print("Seleccione una opción: ");
+
+            opcion = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcion) {
+                case 1:
+
+                    System.out.println();
+                    System.out.println("----- NUEVO CONTACTO -----");
+
+                    System.out.print("Nombre   : ");
+                    String nombre = scanner.nextLine();
+
+                    System.out.print("Teléfono : ");
+                    String telefono = scanner.nextLine();
+
+                    System.out.print("Correo   : ");
+                    String correo = scanner.nextLine();
+
+                    Contacto contacto = new Contacto(nombre, telefono, correo);
+
+                    contactoDAO.guardar(contacto);
+
+                    break;
+                case 2:
+
+                    contactoDAO.listar();
+
+                    break;
+
+                case 3:
+                    System.out.println();
+                    System.out.println("Gracias por utilizar la Agenda de Contactos.");
+                    System.out.println("Programa finalizado.");
+
+                    break;
+                default:
+
+                    System.out.println();
+                    System.out.println("Opción no válida .");
+            }
+        } while (opcion != 3);
+
+        scanner.close();
     }
-}
+}   
